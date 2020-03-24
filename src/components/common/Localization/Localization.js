e5d9cf4163a91bdbd9eb328c68dbe14e42fa1aa6
Roman Zakrevskyi <romul3003@gmail.com>
2020-03-24 18:34:52 +0200

    Add localization, header - desktop

--- src/components/common/Localization/Localization.js
+++ src/components/common/Localization/Localization.js
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Localization.scss'
 import { languagesList } from '../../../mockData/mockData'
+import { useMediaSize } from '../../../hooks/useMediaSize'
 
 const Localization = () => {
+	const [isVisible, setVisible] = useState(false)
+	const isMobile = useMediaSize()
+
+	const showLanguages = () => {
+		setVisible(!isVisible)
+	}
+
 	const languages = () =>
 		languagesList.map(lang => (
-			<li
+			<div
 				className={`localization__item ${lang.active ? 'active' : ''}`.trim()}
 				key={lang.id}
 			>
-				<button type="button" className="localization__btn">
+				<button type="button" className="localization__btn" onClick={() => {}}>
 					<img src={lang.src} alt={lang.label} className="localization__img" />
 				</button>
-			</li>
+			</div>
 		))
 
-	return <ul className="localization">{languages()}</ul>
+	return (
+		<div className={`localization ${!isVisible ? '' : 'visible'}`.trim()}>
+			{languages()}
+			{!isMobile && <button className="localization__more-btn" onClick={showLanguages} />}
+		</div>
+	)
 }
 
 export default Localization
