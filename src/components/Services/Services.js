5d83b107e2de81c61b3624a42185ed3f7706b571
Roman Zakrevskyi <romul3003@gmail.com>
2020-03-26 15:20:58 +0200

    Add gallery desktop

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -6,10 +6,14 @@ const Services = () => {
 	return (
 		<section className="services">
 			<h2 className="services__title title">Услуги</h2>
-			<Gallery />
-			<button type="button" className="services__cost-btn">
-				Расчет стоимости
-			</button>
+			<div className="container">
+				<Gallery />
+				<div className="services__btn-wrap">
+					<button type="button" className="services__cost-btn">
+						Быстрый расчет цены по чертежу
+					</button>
+				</div>
+			</div>
 		</section>
 	)
 }
